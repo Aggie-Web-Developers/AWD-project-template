7e05e9a55625b1b8a2d8c8e1ad155c45348cb60b
Vignesh Joglekar <rexvigasaurus@gmail.com>
2021-02-25 20:00:55 -0600

    Add about us page, and add to navbar

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function (req, res) {
 	res.render('index');
 });
 
+router.get('/about', function (req, res) {
+	res.render('about');
+});
+
 router.get('/terms-and-conditions', function (req, res) {
 	res.render('terms-and-conditions');
 });
